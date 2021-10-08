4cef3e3a7d22dece2a4c58a8af3e9295fd27df09
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2021-10-08 14:16:38 +0300

    Update Lab2_Hard.cs

--- Lab2_Hard.cs
+++ Lab2_Hard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Lab2_Medium
 {
@@ -7,11 +8,43 @@ namespace Lab2_Medium
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Medium12();
+            /*
             List<string> lis = new List<string>
             {
                 "1", "1.", "square"
             };
+            */
+
+            Dictionary<string, string> figure = new Dictionary<string, string>();
+            //List<string> keys = ["1","1", "square"];
+            //figure.Add(keys, "Square");
+            figure.Add("1", "Square");
+            figure.Add("1.", "Square");
+            figure.Add("square", "Square");
+
+            figure.Add("2", "Circle");
+            figure.Add("2.", "Circle");
+            figure.Add("circle", "Circle");
+
+            figure.Add("3", "Equilateral Triangle");
+            figure.Add("3.", "Equilateral Triangle");
+            figure.Add("triangle", "Equilateral Triangle");
+
+            Console.WriteLine(figure.ContainsValue("Square"));
+            Console.WriteLine(figure.ContainsKey("Square"));
+            Console.WriteLine(figure.ContainsKey("1"));
+            Console.WriteLine(figure.ContainsKey("1."));
+            Console.WriteLine(figure["triangle"]);
+
+            string test = "sqUaRe";
+
+            if (figure.ContainsKey(test.ToLower()))
+            {
+                Console.WriteLine(figure[test.ToLower()]);
+            }
             
+
         }
         //static private void Medium10()
         //{
@@ -26,6 +59,19 @@ namespace Lab2_Medium
             double r = 4, area = 0;
             string choice = "";
 
+            Dictionary<string, string> figure = new Dictionary<string, string>();
+            figure.Add("1", "Square");
+            figure.Add("1.", "Square");
+            figure.Add("square", "Square");
+
+            figure.Add("2", "Circle");
+            figure.Add("2.", "Circle");
+            figure.Add("circle", "Circle");
+
+            figure.Add("3", "Equilateral Triangle");
+            figure.Add("3.", "Equilateral Triangle");
+            figure.Add("triangle", "Equilateral Triangle");
+
             Console.WriteLine("How many r values do you want to enter?");
             Console.Write("n =");
             try
@@ -37,7 +83,7 @@ namespace Lab2_Medium
                 Console.WriteLine(f.Message);
             }
 
-            while (n-- > 0)
+            while (n --> 0)
             {
                 try
                 {
@@ -51,9 +97,9 @@ namespace Lab2_Medium
                 }
 
                 Console.WriteLine("Choose what do you want to calculate with r:\n\n" +
-                    "1. Area of square with side r.\n" +
-                    "2. Area of circle with radius r.\n" +
-                    "3. Area of equilateral triangle (sides are equal) with side r.\n\n" +
+                    "1. Area of Square with side r.\n" +
+                    "2. Area of Circle with radius r.\n" +
+                    "3. Area of Equilateral Triangle (sides are equal) with side r.\n\n" +
                     "You can enter either number of your choice or name of figure (square, circle, triangle).\n\n");
                 choice = Console.ReadLine();
 
@@ -78,6 +124,10 @@ namespace Lab2_Medium
                         Console.WriteLine("Wrong input of your choice...");
                         break;
                 }
+
+                Console.WriteLine($"Area of " +
+                    $"{(figure.ContainsKey(choice.ToLower()) ? figure[choice.ToLower()] : "")}" +
+                    $" S = {area}");
                 //Console.WriteLine("Area of {0} == {1}",
                 //    (choice == "1" || choice == "1." || )?:);
             }
