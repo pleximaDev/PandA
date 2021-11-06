83b27477dfedc688fb086e4719463a6d23e4a54e
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2021-11-07 00:55:07 +0300

    Update Lab3_Easy.cs

--- Lab3_Easy.cs
+++ Lab3_Easy.cs
@@ -4,9 +4,12 @@ namespace Lab3_Easy
 {
     class Program
     {
+        private const int
+            lbound = -10, /* left bound of randomization */
+            rbound = 10; /* right bound of randomization */
         static void Main(string[] args)
         {
-            string border = new string('-', 30);
+            string border = new string('-', 45);
             Console.WriteLine(border);
             Console.WriteLine("Task #7:");
             Easy7();
@@ -21,7 +24,7 @@ namespace Lab3_Easy
 
         static private void Easy7()
         {
-            /* Replace elements with zeroes of the array (of size 7)
+            /* Replace elements of the array (of size 7) with zeroes
              * that are more than average value of array elements */
             const int SIZE = 7;
             double[] array = new double[SIZE];
@@ -33,23 +36,40 @@ namespace Lab3_Easy
             Console.Write("Original array: [");
             for (int i = 0; i < SIZE; ++i)
             {
-                array[i] = rand.Next(-10, 10);
+                array[i] = rand.Next(lbound, rbound);
                 Console.Write($" {array[i]} ");
                 average += array[i];
             }
             Console.WriteLine("]");
+
             average /= SIZE;
-            Console.WriteLine($"\nAverage == {average}\n");
+
+            Console.WriteLine($"\nAverage == " +
+                (average % 1 == 0 ?
+                $"{average}" : $"{average,0:F4}") +
+                $"\n");
 
             for (int i = 0; i < SIZE; ++i)
-                if (array[i] > average) array[i] = 0;
+                if (array[i] > average) array[i] = double.NaN;
 
-            writeArray(array);
+            //writeArray(array);
+            Console.Write($"Array after replacement with zeroes:\n[ ");
+            for (int i = 0; i < SIZE; ++i)
+                if (double.IsNaN(array[i]))
+                {
+                    array[i] = 0;
+                    ColorString($" {array[i]} ",
+                        ConsoleColor.Black, ConsoleColor.Green);
+                }
+                else
+                    Console.Write($" {array[i]} ");
+            Console.WriteLine($" ]");
         }
 
         static private void Easy8()
         {
-            /* Count amount of negative elements of the one-dimensional array of size 6 */
+            /* Count amount of negative elements of
+             * the one-dimensional array of size 6 */
             const int SIZE = 6;
             double[] array = new double[SIZE];
             int negativeCount = 0;
@@ -59,16 +79,12 @@ namespace Lab3_Easy
             /* Input */
             Console.Write("Original array: [");
             for (int i = 0; i < SIZE; ++i)
-            {
-                array[i] = rand.Next(-10, 10);
-                Console.Write($" {array[i]} ");
-            }
+                Console.Write($" {array[i] = rand.Next(lbound, rbound)} ");
             Console.WriteLine("]");
 
             for (int i = 0; i < SIZE; ++i)
                 if (array[i] < 0) ++negativeCount;
             Console.WriteLine($"Amount of negative numbers: {negativeCount}");
-
         }
 
         static private void Easy9()
@@ -87,19 +103,29 @@ namespace Lab3_Easy
             /* Input */
             Console.Write("Original array: [");
             for (int i = 0; i < SIZE; ++i)
-            {
-                array[i] = rand.Next(-10, 10);
-                Console.Write($" {array[i]} ");
-            }
+                Console.Write($" {array[i] = rand.Next(lbound, rbound)} ");
             Console.WriteLine("]");
 
             for (int i = 0; i < SIZE; ++i)
                 average += array[i];
             average /= SIZE;
 
+            Console.Write($"\n[ ");
             for (int i = 0; i < SIZE; ++i)
-                if (array[i] > average) ++moreThanAverage;
-            Console.WriteLine($"\nAverage value is: {average}\n\n" +
+                if (array[i] > average)
+                {
+                    ++moreThanAverage;
+                    ColorString($" {array[i]} ",
+                        ConsoleColor.Black, ConsoleColor.Green);
+                }
+                else
+                    Console.Write($" {array[i]} ");
+            Console.WriteLine($" ]");
+
+            Console.WriteLine($"\nAverage value is: " +
+                (average % 1 == 0 ?
+                $"{average}" : $"{average,0:F4}") +
+                "\n\n" +
                 $"Amount of elements that are more " +
                 $"than average value of array elements: {moreThanAverage}");
         }
@@ -110,6 +136,17 @@ namespace Lab3_Easy
             for (int i = 0; i < array.Length; ++i)
                 Console.Write($" {array[i]} ");
             Console.WriteLine("]");
+        }
+
+        static private void ColorString
+            (string inputString,
+            System.ConsoleColor backColor,
+            System.ConsoleColor foreColor)
+        {
+            Console.BackgroundColor = backColor;
+            Console.ForegroundColor = foreColor;
+            Console.Write($"{inputString}");
+            Console.ResetColor();
         }
     }
 }
